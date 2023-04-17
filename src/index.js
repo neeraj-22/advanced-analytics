d0609552e7dd8de6abf55e7e4a26a45059eccd9c
neeraj-22 <gandhineeraj48@gmail.com>
2023-04-17 13:06:05 +0530

    ga

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes.js'
 
+
+
 ReactDOM.render(<Routes/>, document.getElementById('root'));
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-EB8KV61KCW')
 const Homepage = () => {
+
+    // useEffect(() => {
+    //     ReactGA.pageview(window.location.pathname + window.location.search) 
+    // })
     return (
         <div>
             Homepage
